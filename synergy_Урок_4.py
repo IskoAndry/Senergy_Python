e8052c054c48ce189590a2f8dee3679f724678ab
isakov <isakov4852@mail.ru>
2024-04-05 20:38:56 +0300

    урок 4

---
+++ synergy_Урок_4.py
@@ -0,0 +1,21 @@
+# УрокN4. float,int арифметические операции
+# Задание № 1
+a = int(input())
+b = int(input())
+S = a * b
+P = (a * 2) + (b * 2)
+print(S)
+print(P)
+
+
+# Задание № 2
+num = 46275
+txt1 = str(num)[0]
+txt2 = str(num)[1]
+txt3 = str(num)[2]
+txt4 = str(num)[3]
+txt5 = str(num)[4]
+
+res = (int(txt4)**int(txt5) * int(txt3))/(int(txt1)-int(txt2))
+
+print(res)
