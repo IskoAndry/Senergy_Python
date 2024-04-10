749827ced679f7e0c1b59b6a6a2bf2ba22ed7924
isakov <isakov4852@mail.ru>
2024-04-10 13:55:20 +0300

    lessen 8

---
+++ synergy_Урок_8.py
@@ -0,0 +1,57 @@
+# Урок N 8.
+# Задание № 1
+
+# n = int(input())
+# tmp = list(map(int, input().split()))
+# tmp.reverse()
+# print(tmp)
+
+
+# Задание № 2
+
+#n = int(input())
+# tmp = list(map(int, input().split()))
+# res = []
+# l = tmp[-1]
+# tmp.insert(0, l)
+# tmp.pop()
+# print(tmp)
+
+
+# Задание № 3
+
+
+
+
+# l = (len(tmp)-1)
+# for i in range(0, len(tmp)):
+#     #print(tmp[i])
+#     if i == l:
+#        res.append(tmp[i])
+# res.append(tmp[i])
+# print(res)
+
+
+#print(tmp.insert(1, ))
+
+# a = [1, 4, 2, 3, 4, 5, 6, 7, 8, 9]
+# a.append(199)
+# a.insert(33)
+# a.pop()
+# #a.clear()
+# print(a.count(4))
+# for i in a:
+#     print(i,  end=',')
+
+#res = list(map(int, input().split()))
+
+# n = int(input())
+# tmp = list(map(int, input().split()))
+# res = []
+
+# for i in range(0, len(tmp)):
+#     if tmp[i] !=50:
+#         res.append(tmp[i])
+# print(res)
+
+# a = [3 for i in range(5)]
